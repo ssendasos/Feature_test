a74f8814fb85833dee7586883c843b5cd5c85dda
Юра <yuriy.fak@mail.ru>
2020-04-11 12:04:58 +0300

    Реворк слайдера в блоке meeting

--- source/js/meeting-slider.js
+++ source/js/meeting-slider.js
@@ -1,16 +1,26 @@
 
-$(document).ready(function() {
-    $(".carousel").slick({
-            mobileFirst: 'true',
-            dots: true,                                               
-            initialSlide: 1,                       
-            prevArrow: '.slick-prev',
-            nextArrow: '.slick-next',
-            responsive: [
-                {
-                    breakpoint: 1139,
-                    settings: "unslick",
-                }
-            ]   
-    });
-});
+const list = document.getElementById('slider-list')
+const leftButton = document.getElementById('left-arrow')
+const rightButton = document.getElementById('right-arrow')
+const items = document.querySelectorAll('.meeting__item')
+const itemsValue = items.length
+let counter = 0
+
+rightButton.addEventListener('click', evt => {
+  evt.preventDefault()
+  if (counter < itemsValue - 1) {
+    counter++
+    list.style.transform = 'translateX(' + (-222 * counter) + 'px';
+    console.log(counter)
+  }
+})
+
+
+leftButton.addEventListener('click', evt => {
+  evt.preventDefault()
+  if (counter > 0) {
+    counter--
+    list.style.transform = 'translateX(' + (-222 * counter) + 'px';
+    
+  }
+})
